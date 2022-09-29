ccc7fd918b427f4b6d952f67621c544deed8c02d
alejandrogh7 <alejo1garciasosa@gmail.com>
2022-09-29 16:07:33 -0500

    feature(product)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { getProducts } from './redux/thunks/productThunks';
 
 //COMPONENTS
 import LandingPage from './Components/LandingPage/LandingPage.jsx';
+import Product from './Components/Product/Product.jsx';
 
 function App() {
   const dispatch = useDispatch();
+  const products = useSelector((state) => state.products);
 
   useEffect(() => {
     dispatch(getProducts());
@@ -16,6 +18,16 @@ function App() {
   return (
     <div className="App">
       <LandingPage />
+      {products.products.map((product) => {
+        return <Product 
+          key={product.id} 
+          id={product.id} 
+          title={product.title} 
+          description={product.description} 
+          product_image={product.product_image} 
+          back_image={product.back_image}
+          />
+      })}
     </div>
   );
 }
--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
-export default function Product() {
+export default function Product({ title, description, product_image, back_image, id }) {
 	return (
-		<section></section>
+		<section className={`product_container ${id % 2 === 0 ? "active" : ""}`}>
+			<div className="image">
+				<img src={ back_image } alt={ title }/>
+				{id === 1 ? <button className="btn">shop</button>: null }
+				<div className={id % 2 === 0 ? "triangle-left" : "triangle-right"}></div>
+			</div>
+			<div className="data">
+				<div className="title">
+					<img src={ product_image } alt={ title }/>
+					<h1>{ title }</h1>
+					<a></a>
+				</div>
+				<div className="data-2">
+					<span className="desc">{ description }</span>
+					<span className="more">> ver mas</span>
+				</div>
+			</div>
+		</section>
 	);
 }
